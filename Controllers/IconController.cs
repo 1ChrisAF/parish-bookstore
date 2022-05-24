4959414b374559c2f4408b0e4e0cd94d42c171e7
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-05-24 15:58:58 -0500

    Layout almost done

---
+++ Controllers/IconController.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using parish_bookstore.Models;
+
+namespace parish_bookstore.Controllers;
+
+public class IconController : Controller 
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
