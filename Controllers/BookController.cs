1f52356c66125e256f400081a8a58d5054a12751
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-05-24 14:27:40 -0500

    Initial BookController commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using parish_bookstore.Models;
+
+namespace parish_bookstore.Controllers;
+
+public class BookController : Controller 
+{
+
+}
