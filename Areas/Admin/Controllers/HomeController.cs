0d755635ee949c882bc3694dbcc0c9bbc1a73b62
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-05-25 18:31:18 -0500

    Edited CSS for tables

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -2,10 +2,12 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using parish_bookstore.Models;
 
-namespace parish_bookstore.Controllers;
+namespace parish_bookstore.Areas.Admin.Controllers;
 
+[Area("Admin")]
 public class HomeController : Controller
 {
+    
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
