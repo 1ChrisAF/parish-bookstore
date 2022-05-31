7437d3cb5136124f4cbb3e517fe06a9d75789aea
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-05-31 18:16:24 -0500

    Updated Context with "unassigned" Category

--- Models/BookstoreContext.cs
+++ Models/BookstoreContext.cs
@@ -43,29 +43,33 @@ public class BookstoreContext : DbContext
             new TempModel {Id = 1, ImageName = ""}
         );
         modelBuilder.Entity<BookCategory>().HasData(
-            new BookCategory {BookCategoryId = 1, CategoryName = "Prayer & Service Books"},
-            new BookCategory {BookCategoryId = 2, CategoryName = "Fathers & Patristics"},
-            new BookCategory {BookCategoryId = 3, CategoryName = "Spiritual Development"},
-            new BookCategory {BookCategoryId = 4, CategoryName = "Church History"},
-            new BookCategory {BookCategoryId = 5, CategoryName = "Liturgics & Services"}
+            new BookCategory {BookCategoryId = 1, CategoryName = "unassigned"},
+            new BookCategory {BookCategoryId = 2, CategoryName = "Prayer & Service Books"},
+            new BookCategory {BookCategoryId = 3, CategoryName = "Fathers & Patristics"},
+            new BookCategory {BookCategoryId = 4, CategoryName = "Spiritual Development"},
+            new BookCategory {BookCategoryId = 5, CategoryName = "Church History"},
+            new BookCategory {BookCategoryId = 6, CategoryName = "Liturgics & Services"}
         );
         modelBuilder.Entity<IconCategory>().HasData(
-            new IconCategory {IconCategoryId = 1, CategoryName = "Christ"},
-            new IconCategory {IconCategoryId = 2, CategoryName = "Theotokos"},
-            new IconCategory {IconCategoryId = 3, CategoryName = "Angels"},
-            new IconCategory {IconCategoryId = 4, CategoryName = "Saints"},
-            new IconCategory {IconCategoryId = 5, CategoryName = "Feasts"}
+            new IconCategory {IconCategoryId = 1, CategoryName = "unassigned"},
+            new IconCategory {IconCategoryId = 2, CategoryName = "Christ"},
+            new IconCategory {IconCategoryId = 3, CategoryName = "Theotokos"},
+            new IconCategory {IconCategoryId = 4, CategoryName = "Angels"},
+            new IconCategory {IconCategoryId = 5, CategoryName = "Saints"},
+            new IconCategory {IconCategoryId = 6, CategoryName = "Feasts"}
         );
         modelBuilder.Entity<HomeAltarItemCategory>().HasData(
-            new HomeAltarItemCategory {HomeAltarItemCategoryId = 1, CategoryName = "Censers"},
-            new HomeAltarItemCategory {HomeAltarItemCategoryId = 2, CategoryName = "Charcoal"},
-            new HomeAltarItemCategory {HomeAltarItemCategoryId = 3, CategoryName = "Incense"},
-            new HomeAltarItemCategory {HomeAltarItemCategoryId = 4, CategoryName = "Vigil Lamps & Supplies"}
+            new HomeAltarItemCategory {HomeAltarItemCategoryId = 1, CategoryName = "unassigned"},
+            new HomeAltarItemCategory {HomeAltarItemCategoryId = 2, CategoryName = "Censers"},
+            new HomeAltarItemCategory {HomeAltarItemCategoryId = 3, CategoryName = "Charcoal"},
+            new HomeAltarItemCategory {HomeAltarItemCategoryId = 4, CategoryName = "Incense"},
+            new HomeAltarItemCategory {HomeAltarItemCategoryId = 5, CategoryName = "Vigil Lamps & Supplies"}
         );
         modelBuilder.Entity<GeneralItemCategory>().HasData(
-            new GeneralItemCategory {GeneralItemCategoryId = 1, CategoryName = "Gifts"},
-            new GeneralItemCategory {GeneralItemCategoryId = 2, CategoryName = "Cross Necklaces"},
-            new GeneralItemCategory {GeneralItemCategoryId = 3, CategoryName = "Seasonal"}
+            new GeneralItemCategory {GeneralItemCategoryId = 1, CategoryName = "unassigned"},
+            new GeneralItemCategory {GeneralItemCategoryId = 2, CategoryName = "Gifts"},
+            new GeneralItemCategory {GeneralItemCategoryId = 3, CategoryName = "Cross Necklaces"},
+            new GeneralItemCategory {GeneralItemCategoryId = 4, CategoryName = "Seasonal"}
         );
     }
     
