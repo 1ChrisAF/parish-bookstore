9793ae323246d06a294a192c4ca97cf2385f9d84
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-06-03 15:40:00 -0500

    Updated Program.cs w/ correct context name

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using parish_bookstore.Data;
+using parish_bookstore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<BookstoreContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<BookstoreContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
