6f1f28e9e19e781d2a03eb1c208f1befda178ae6
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-06-03 15:15:57 -0500

    Completed Category models

---
+++ Models/PrayerRopeCategory.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace parish_bookstore.Models;
+
+public class PrayerRopeCategory
+{
+    public int CategoryId {get; set;}
+    public string? CategoryName {get; set;}
+}
