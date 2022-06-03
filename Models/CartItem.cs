f5dea855d1e3eeca6443c303c89c8790d905f2a0
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-06-03 15:20:36 -0500

    Completed User and CartItem models

---
+++ Models/CartItem.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace parish_bookstore.Models;
+
+public class CartItem
+{
+    public int CartItemId {get; set;}
+    public int UserId {get; set;}           // Identifies the user to whom the Cart belongs
+    public int StoreItemId {get; set;}      // Identifies the object type
+    public int ItemId {get; set;}           // Identifies the specific instance of the object type
+    public int Quantity {get; set;}         // Desired quantity of instances of objects of a certain type
+}
